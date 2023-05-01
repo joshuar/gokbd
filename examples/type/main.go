824dce41baf6e1720ed2dfef16748555e9e5aa7c
Joshua Rich <joshua.rich@gmail.com>
2023-05-01 18:41:37 +1000

    fix(examples): add delays to see the results of the example more clearly

--- examples/type/main.go
+++ examples/type/main.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2023 Joshua Rich <joshua.rich@gmail.com>
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -7,6 +7,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/joshuar/gokbd"
 	"github.com/rs/zerolog"
@@ -26,13 +27,17 @@ func main() {
 	vDev.TypeRune('l')
 	vDev.TypeRune('o')
 	vDev.TypeSpace()
+	time.Sleep(time.Millisecond * 500)
 	vDev.TypeString("there!")
 	vDev.TypeSpace()
 	vDev.TypeString("I made a misteak")
+	time.Sleep(time.Second)
 	for i := 0; i <= 2; i++ {
 		vDev.TypeBackspace()
 	}
+	time.Sleep(time.Second)
 	vDev.TypeString("ake")
+	time.Sleep(time.Second * 2)
 
 	vDev.Close()
 }
