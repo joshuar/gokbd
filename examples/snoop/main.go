7b53e19f6bbbf911c1af0cba760e7c0823e4b6a5
Joshua Rich <joshua.rich@gmail.com>
2023-05-03 10:03:42 +1000

    feat(examples): rewrite snoop example to use a virtual keyboard

--- examples/snoop/main.go
+++ examples/snoop/main.go
@@ -19,16 +19,31 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 
-	keys := gokbd.SnoopAllKeyboards(gokbd.OpenKeyboardDevices())
-	for k := range keys {
-		if k.Value == 1 && k.TypeName == "EV_KEY" {
-			log.Debug().Msgf("Key pressed: %s %s %d %c", k.TypeName, k.EventName, k.Value, k.AsRune)
-		}
-		if k.Value == 0 && k.TypeName == "EV_KEY" {
-			log.Debug().Msgf("Key released: %s %s %d", k.TypeName, k.EventName, k.Value)
-		}
-		if k.Value == 2 && k.TypeName == "EV_KEY" {
-			log.Debug().Msgf("Key held: %s %s %d %c", k.TypeName, k.EventName, k.Value, k.AsRune)
-		}
+	vDev, err := gokbd.NewVirtualKeyboard("gokbd test")
+	if err != nil {
+		log.Panic().Msg("Could not create a virtual keyboard!")
 	}
+	kbd, err := gokbd.OpenKeyboardDevice(vDev.Device())
+	if err != nil {
+		log.Panic().Msg("Could not access virtual keyboard!")
+	}
+
+	keys := gokbd.SnoopKeyboard(kbd)
+
+	// keys := gokbd.SnoopAllKeyboards(gokbd.OpenKeyboardDevices())
+	go func() {
+		for k := range keys {
+			if k.Value == 1 && k.TypeName == "EV_KEY" {
+				log.Debug().Msgf("Key pressed: %s %s %d %c", k.TypeName, k.EventName, k.Value, k.AsRune)
+			}
+			if k.Value == 0 && k.TypeName == "EV_KEY" {
+				log.Debug().Msgf("Key released: %s %s %d", k.TypeName, k.EventName, k.Value)
+			}
+			if k.Value == 2 && k.TypeName == "EV_KEY" {
+				log.Debug().Msgf("Key held: %s %s %d %c", k.TypeName, k.EventName, k.Value, k.AsRune)
+			}
+		}
+	}()
+	vDev.TypeString("Hello there!")
+	vDev.Close()
 }
